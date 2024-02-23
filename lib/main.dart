04600a728e56f447f2dac216c15b2782cdeee790
jimxdxd <144504691+Jimwelljanxdxd@users.noreply.github.com>
2024-02-23 15:40:39 +0800

    Banas, Jimwelljan
    added story

--- lib/main.dart
+++ lib/main.dart
@@ -64,5 +64,123 @@ class ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateMi
         ),
       ),
 
-      
-
+        body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Row 1
+            Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey[300],
+                          ),
+                          padding: EdgeInsets.all(14),
+                          child: Icon(
+                            Icons.camera_alt,
+                            size: 20,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Positioned(
+                          right: -2,
+                          bottom: -1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            padding: EdgeInsets.all(1),
+                            child: Icon(
+                              Icons.add_circle_outline,
+                              size: 13,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Add a story',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(10, (index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 55,
+                                height: 55,
+                                child: Stack(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.green[300],
+                                      ),
+                                      padding: EdgeInsets.all(2),
+                                      child: ClipOval(
+                                        child: Image.network(
+                                          'https://assets-global.website-files.com/63bb7d51076d755de475e3a3/63be71febd2de358b87f9b6f_Jennie-Kim.webp',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      right: 0,
+                                      bottom: 2,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.green,
+                                        ),
+                                        padding: EdgeInsets.all(1),
+                                        child: Icon(
+                                          Icons.zoom_in,
+                                          size: 13,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Cool',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 7),
+          ],
+        ),
+      ),
